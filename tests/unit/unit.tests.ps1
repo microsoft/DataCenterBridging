84809c41d77355c078a4d5115eba9da81d93b5ab
dcuomo <dcuomo@outlook.com>
2019-05-28 19:20:25 -0700

    Updating to use repoName env var

--- tests/unit/unit.tests.ps1
+++ tests/unit/unit.tests.ps1
@@ -1,13 +1,13 @@
-$DataFile   = Import-PowerShellDataFile ".\$($env:APPVEYOR_BUILD_FOLDER).psd1" -ErrorAction SilentlyContinue
-$TestModule = Test-ModuleManifest       ".\$($env:APPVEYOR_BUILD_FOLDER).psd1" -ErrorAction SilentlyContinue
+$DataFile   = Import-PowerShellDataFile .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
+$TestModule = Test-ModuleManifest       .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
 
 Describe "$($env:APPVEYOR_BUILD_FOLDER)-Manifest" {
     Context Validation {
-        It "[Import-PowerShellDataFile] - $($env:APPVEYOR_BUILD_FOLDER).psd1 is a valid PowerShell Data File" {
+        It "[Import-PowerShellDataFile] - $($env:repoName).psd1 is a valid PowerShell Data File" {
             $DataFile | Should Not BeNullOrEmpty
         }
 
-        It "[Test-ModuleManifest] - $($env:APPVEYOR_BUILD_FOLDER).psd1 should pass the basic test" {
+        It "[Test-ModuleManifest] - $($env:repoName).psd1 should pass the basic test" {
             $TestModule | Should Not BeNullOrEmpty
         }
     }
