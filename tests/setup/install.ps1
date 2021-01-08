605d2bb2a4058ff2b7f7c14217035434c6e29880
Dan Cuomo <dcuomo@outlook.com>
2021-01-07 16:26:06 -0800

    copying install file from vDCB

--- tests/setup/install.ps1
+++ tests/setup/install.ps1
@@ -1,22 +1,60 @@
-git clone -q https://github.com/PowerShell/DscResource.Tests
+git.exe clone -q https://github.com/PowerShell/DscResource.Tests
 
 Import-Module -Name "$env:APPVEYOR_BUILD_FOLDER\DscResource.Tests\AppVeyor.psm1"
 Invoke-AppveyorInstallTask
 
+[string[]]$PowerShellModules = @("Pester", 'posh-git', 'PSScriptAnalyzer')
+
 $ModuleManifest = Test-ModuleManifest .\$($env:RepoName).psd1 -ErrorAction SilentlyContinue
 $repoRequiredModules = $ModuleManifest.RequiredModules.Name
+$repoRequiredModules += $ModuleManifest.PrivateData.PSData.ExternalModuleDependencies
 
-[string[]]$PowerShellModules = @('posh-git', 'PSScriptAnalyzer')
+If ($repoRequiredModules) { $PowerShellModules += $repoRequiredModules }
 
-if ($repoRequiredModules) { $PowerShellModules += $repoRequiredModules }
+# This section is taken care of by Invoke-AppVeyorInstallTask
+<#[string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
 
-# Install the PowerShell Modules
-Install-Module Pester -RequiredVersion '4.9.0' -Force -Confirm:$false -SkipPublisherCheck -AllowClobber -Repository PSGallery
+# Install package providers for PowerShell Modules
+ForEach ($Provider in $PackageProviders) {
+    If (!(Get-PackageProvider $Provider -ErrorAction SilentlyContinue)) {
+        Install-PackageProvider $Provider -Force -ForceBootstrap -Scope CurrentUser
+    }
+}#>
+
+# Feature Installation
+
+$serverFeatureList = @('Hyper-V')
+
+If ($PowerShellModules -contains 'FailoverClusters') {
+    $serverFeatureList += 'RSAT-Clustering-Mgmt', 'RSAT-Clustering-PowerShell'
+}
+
+$BuildSystem = Get-CimInstance -ClassName 'Win32_OperatingSystem'
 
 ForEach ($Module in $PowerShellModules) {
-    If (!(Get-Module -ListAvailable $Module -ErrorAction SilentlyContinue)) {
-        Install-Module $Module -Scope CurrentUser -Force -Repository PSGallery
+    If ($Module -eq 'FailoverClusters') {
+        Switch -Wildcard ($BuildSystem.Caption) {
+            '*Windows 10*' {
+                Write-Output 'Build System is Windows 10'
+                Write-Output "Not Implemented"
+
+                # Get FailoverCluster Capability Name and Install on W10 Builds
+                $capabilityName = (Get-WindowsCapability -Online | Where-Object Name -like *RSAT*FailoverCluster.Management*).Name
+                Add-WindowsCapability -Name $capabilityName -Online
+            }
+
+            Default {
+                Write-Output "Build System is $($BuildSystem.Caption)"
+                Install-WindowsFeature -Name $serverFeatureList -IncludeManagementTools | Out-Null
+            }
+        }
     }
-    
-    Import-Module $Module
+    ElseIf ($Module -eq 'Pester') {
+        Install-Module $Module -Scope AllUsers -Force -Repository PSGallery -AllowClobber -SkipPublisherCheck -RequiredVersion 4.9.0
+        Import-Module $Module -RequiredVersion 4.9.0
+    }
+    else {
+        Install-Module $Module -Scope AllUsers -Force -Repository PSGallery -AllowClobber
+        Import-Module $Module
+    }
 }
