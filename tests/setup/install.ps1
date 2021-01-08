be8f759ac05046efe546ff051001edc2043b2de8
Dan Cuomo <dcuomo@outlook.com>
2021-01-07 16:00:55 -0800

    fix for pester compat issues

--- tests/setup/install.ps1
+++ tests/setup/install.ps1
@@ -1,24 +1,13 @@
 git clone -q https://github.com/PowerShell/DscResource.Tests
 
 Import-Module -Name "$env:APPVEYOR_BUILD_FOLDER\DscResource.Tests\AppVeyor.psm1"
+Import-Module -Name Pester -RequiredVersion '4.9.0' -Force
 Invoke-AppveyorInstallTask
-
-[string[]]$PowerShellModules = @('posh-git', 'psake', 'poshspec', 'PSScriptAnalyzer')
 
 $ModuleManifest = Test-ModuleManifest .\$($env:RepoName).psd1 -ErrorAction SilentlyContinue
 $repoRequiredModules = $ModuleManifest.RequiredModules.Name
 
 if ($repoRequiredModules) { $PowerShellModules += $repoRequiredModules }
-
-# This section is taken care of by Invoke-AppVeyorInstallTask
-<#[string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
-
-# Install package providers for PowerShell Modules
-ForEach ($Provider in $PackageProviders) {
-    If (!(Get-PackageProvider $Provider -ErrorAction SilentlyContinue)) {
-        Install-PackageProvider $Provider -Force -ForceBootstrap -Scope CurrentUser
-    }
-}#>
 
 # Install the PowerShell Modules
 Install-Module Pester -RequiredVersion '4.9.0' -Force -Confirm:$false -SkipPublisherCheck -AllowClobber -Scope CurrentUser -Repository PSGallery
